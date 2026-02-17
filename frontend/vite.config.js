5b3c210954476945370e4b104c939ab42dd8ec83
gom0ro <gom0ro>
2026-02-17 23:04:01 +0500

    Build frontend and update docs for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base: '/m/',
+    base: '/ma/',
     plugins: [vue()],
     server: {
         port: 3000,
